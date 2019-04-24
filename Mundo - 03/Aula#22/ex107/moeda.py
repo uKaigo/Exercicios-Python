dda08cb20a370176091afef8459663f1fa912cbc
Kaigo <37600692+Kaigo11k@users.noreply.github.com>
2019-04-24 14:58:02 -0300

    Add files via upload

---
+++ Mundo - 03/Aula#22/ex107/moeda.py
@@ -0,0 +1,11 @@
+def metade(coin):
+    return coin/2
+
+def dobro(coin):
+    return coin*2
+
+def aumentar(coin, pcent):
+    return coin + (coin*pcent/100)
+
+def diminuir(coin, pcent):
+    return coin - (coin*pcent/100)
