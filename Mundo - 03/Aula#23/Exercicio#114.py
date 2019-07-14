81068d7806a689f2fa1d627f0c860550615ff150
Kaigo <37600692+Kaigo11k@users.noreply.github.com>
2019-07-14 01:06:02 -0300

    Aula 22

--- Mundo - 03/Aula#23/Exercicio#114.py
+++ Mundo - 03/Aula#23/Exercicio#114.py
@@ -4,4 +4,4 @@ try:
 except:
     print("\033[31mO site Pudim não está acessível no momento.")
 else:
-    print("\033[32mConsegui acessar o site Pudim com sucesso!") 
+    print("\033[32mConsegui acessar o site Pudim com sucesso!")
